9933b2b08293bfd1a255d85744dd9a1e16894703
Suraj K Suresh <kssuraj15@gmail.com>
2021-03-18 06:15:02 +0530

    Format call_cust.cpp

--- LogMaker/FunctionTracer/call_cust.cpp
+++ LogMaker/FunctionTracer/call_cust.cpp
@@ -1,175 +1,79 @@
-
 #include "pin.H"
 #include <iostream>
-#include <fstream>
+#include <vector>
+#include <string.h> 
+#include "CallTracer.h"
+
+using std::endl;
+using std::cerr;
+using std::ios;
+using std::string;
+using std::stringstream;
 
 
-/* ===================================================================== */
-/* Global Variables */
-/* ===================================================================== */
-
-std::ofstream TraceFile;
 std::vector<string> calls_list;
-std::map<std::string, int> countMap;
-
-/* ===================================================================== */
-/* Commandline Switches */
-/* ===================================================================== */
-
-KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "calltrace.out", "specify trace file name");
-KNOB<BOOL>   KnobPrintArgs(KNOB_MODE_WRITEONCE, "pintool", "a", "0", "print call arguments ");
-KNOB<BOOL>   KnobAllCalls(KNOB_MODE_WRITEONCE, "pintool", "c", "0", "print all calls ");
-
-
-/* ===================================================================== */
-/* Print Help Message                                                    */
-/* ===================================================================== */
-
-INT32 Usage()
-{
-    cerr << "This tool produces a call trace." << endl << endl;
-    cerr << KNOB_BASE::StringKnobSummary() << endl;
-    return -1;
-}
+std::map<string, int> countMap;
 
 string invalid = "$";
 
-/* ===================================================================== */
+INT32 Usage()
+{
+    cerr << "This tool counts the number of dynamic instructions executed" << endl;
+    cerr << endl << KNOB_BASE::StringKnobSummary() << endl;
+    return -1;
+}
+
+//Convert address to function name
+
 const string *Target2String(ADDRINT target)
 {
+    stringstream tmp_stream;
     string name = RTN_FindNameByAddress(target);
-    if (name == "")
-        return &invalid;
+    if (name == ""){
+        name = "sub_";
+        tmp_stream << name << std::hex << target;
+        tmp_stream >> name;
+        return new string(name);
+    }
     else
         return new string(name);
 }
 
-/* ===================================================================== */
-
-VOID  do_call_args(const string *s, ADDRINT arg0)
-{
-    TraceFile << *s << "(" << arg0 << ",...)" << endl;
-}
-
-/* ===================================================================== */
-
-VOID  do_call_args_indirect(ADDRINT target, BOOL taken, ADDRINT arg0)
-{
-    if( !taken ) return;
-    
-    const string *s = Target2String(target);
-    do_call_args(s, arg0);
-
-    if (s != &invalid)
-        delete s;
-}
-
-/* ===================================================================== */
-
-VOID  do_call(const string *s)
+VOID do_call(const string *s)
 {
     // Here we check for _Exit because that's the last call happening before the binary teminates.
-    if(*s != "_Exit")
-        calls_list.push_back(*s);
-    else if(*s == "_Exit"){ 
-        for(unsigned int i = 0; i < calls_list.size();i++)
-        {
-            string ser = calls_list[i];
-            //Switch cases for parsing the list we have 
-            switch (ser[0])
-            {
-            case '.':
-                break;
-            case '_':
-                break;
-            case '$':
-                break;
-            case '(':
-                break;
-            case '#':
-                break;
-            case '@':
-                break;
-            default:
-                //cout << ser[0] << endl;
-                //TraceFile << ser << endl;
-                auto result = countMap.insert(std::pair<std::string, int>(ser, 1));
-	            if (result.second == false)
-		            result.first->second++;
-                break;
-            }
-        }
-        if ( std::find(calls_list.begin(), calls_list.end(), "ptrace@plt") != calls_list.end() ) 
-        {
-            cout << "================Analysis Complete================"<<endl;
-            cout << "PTRACE detected" <<endl;
-            cout << "======================Stats ====================="<<endl;
-        }
-        for (auto & elem : countMap){
-	    // If frequency count is greater than 1 then its a duplicate element
-	        if (elem.second >= 1){
-		        std::cout << elem.first << " was called " << elem.second << " times."<< std::endl;
-	        }
-        }
-    }
+    calls_list.push_back(*s);
 }
-
-
-/* ===================================================================== */
 
 VOID  do_call_indirect(ADDRINT target, BOOL taken)
 {
     if( !taken ) return;
 
     const string *s = Target2String(target);
-    do_call( s );
+    do_call(s);
     
     if (s != &invalid)
         delete s;
 }
 
-/* ===================================================================== */
-
 VOID Trace(TRACE trace, VOID *v)
-{
-    const BOOL print_args = KnobPrintArgs.Value();
-    
-        
+{   
     for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl))
     {
         INS tail = BBL_InsTail(bbl);
-        
+        //const char * disasm;
         if( INS_IsCall(tail) )
         {
-            if( INS_IsDirectBranchOrCall(tail) )
+            if( INS_IsDirectControlFlow(tail) )
             {
-                const ADDRINT target = INS_DirectBranchOrCallTargetAddress(tail);
-                if( print_args )
-                {
-                    INS_InsertPredicatedCall(tail, IPOINT_BEFORE, AFUNPTR(do_call_args),
-                                             IARG_PTR, Target2String(target), IARG_FUNCARG_CALLSITE_VALUE, 0, IARG_END);
-                }
-                else
-                {
-                    INS_InsertPredicatedCall(tail, IPOINT_BEFORE, AFUNPTR(do_call),
-                                             IARG_PTR, Target2String(target), IARG_END);
-                }
-                
+                ADDRINT target = INS_DirectControlFlowTargetAddress(tail);
+                INS_InsertPredicatedCall(tail, IPOINT_BEFORE, AFUNPTR(do_call),
+                                            IARG_PTR,Target2String(target),IARG_END);
             }
             else
             {
-                if( print_args )
-                {
-                    INS_InsertCall(tail, IPOINT_BEFORE, AFUNPTR(do_call_args_indirect),
-                                   IARG_BRANCH_TARGET_ADDR, IARG_BRANCH_TAKEN,  IARG_FUNCARG_CALLSITE_VALUE, 0, IARG_END);
-                }
-                else
-                {
-                    INS_InsertCall(tail, IPOINT_BEFORE, AFUNPTR(do_call_indirect),
-                                   IARG_BRANCH_TARGET_ADDR, IARG_BRANCH_TAKEN, IARG_END);
-                }
-                
-                
+            INS_InsertCall(tail, IPOINT_BEFORE, AFUNPTR(do_call_indirect),
+                            IARG_BRANCH_TARGET_ADDR, IARG_BRANCH_TAKEN, IARG_END);
             }
         }
         else
@@ -178,37 +82,60 @@ VOID Trace(TRACE trace, VOID *v)
             RTN rtn = TRACE_Rtn(trace);
             
             // also track stup jumps into share libraries
-            if( RTN_Valid(rtn) && !INS_IsDirectBranchOrCall(tail) && ".plt" == SEC_Name( RTN_Sec( rtn ) ))
+            if( RTN_Valid(rtn) && !INS_IsDirectControlFlow(tail) && ".plt" == SEC_Name( RTN_Sec( rtn ) ))
             {
-                if( print_args )
-                {
-                    INS_InsertCall(tail, IPOINT_BEFORE, AFUNPTR(do_call_args_indirect),
-                                   IARG_BRANCH_TARGET_ADDR, IARG_BRANCH_TAKEN,  IARG_FUNCARG_CALLSITE_VALUE, 0, IARG_END);
-                }
-                else
-                {
-                    INS_InsertCall(tail, IPOINT_BEFORE, AFUNPTR(do_call_indirect),
-                                   IARG_BRANCH_TARGET_ADDR, IARG_BRANCH_TAKEN, IARG_END);
-
-                }
+                INS_InsertCall(tail, IPOINT_BEFORE, AFUNPTR(do_call_indirect),
+                                   IARG_BRANCH_TARGET_ADDR, IARG_BRANCH_TAKEN,IARG_END);
             }
         }
         
     }
 }
 
-/* ===================================================================== */
-
 VOID Fini(INT32 code, VOID *v)
 {
-    TraceFile << "# eof" << endl;
-    
-    TraceFile.close();
-}
+    string ser;
+    for(unsigned int i = 0; i < calls_list.size();i++)
+    {
+        ser = calls_list[i];
+        
+        //Switch cases for parsing the list we have 
+        switch (ser[0])
+        {
+        case '.':
+            break;
+        case '_':
+            break;
+        case '$':
+            break;
+        case '(':
+            break;
+        case '#':
+            break;
+        case '@':
+            break;
+        default:
+            auto result = countMap.insert(std::pair<string, int>(ser, 1));
+	        if (result.second == false)
+	            result.first->second++;
+            break;
+        }
+    }
+    printf("[+] Analysis Complete\n");
+    if ( std::find(calls_list.begin(), calls_list.end(), "ptrace@plt") != calls_list.end() ) 
+    {
+        printf("[+] PTRACE detected");
+    }
+    printf("[+] Function call details\n");
+    printf("%-40s%-25s%-5s\n", "Function Name", "Times Called", "Address");
+    for (auto & elem : countMap){
+    // If frequency count is greater than 1 then its a duplicate element
+	if (elem.second >= 1){
+        printf("%-40s%-25d%-5s\n", elem.first.c_str(), elem.second, "Address");
+	}
+    }
 
-/* ===================================================================== */
-/* Main                                                                  */
-/* ===================================================================== */
+}
 
 int  main(int argc, char *argv[])
 {
@@ -220,19 +147,6 @@ int  main(int argc, char *argv[])
         return Usage();
     }
     
-
-    TraceFile.open(KnobOutputFile.Value().c_str());
-
-    TraceFile << hex;
-    TraceFile.setf(ios::showbase);
-    
-    string trace_header = string("#\n"
-                                 "# Call Trace Generated By Pin\n"
-                                 "#\n");
-    
-
-    TraceFile.write(trace_header.c_str(),trace_header.size());
-    
     TRACE_AddInstrumentFunction(Trace, 0);
     PIN_AddFiniFunction(Fini, 0);
 
@@ -242,7 +156,3 @@ int  main(int argc, char *argv[])
     
     return 0;
 }
-
-/* ===================================================================== */
-/* eof */
-/* ===================================================================== */
